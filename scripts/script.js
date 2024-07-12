b1d307d1b8ff9144074e1962b51bbf3e51938f44
Christopher Jamieson <shift16@protonmail.com>
2024-07-12 15:58:32 -0400

    Updated script.js so that it can dynamically load content into the main page and also updated index.html so that it is easier to do that

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,25 @@
-// Add code kthx
+const buttons = document.querySelectorAll(".text-link", ".image-link")
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        switch (button.id) {
+            case "home-page":
+                fetch("/pages/home/index.html")
+                    .then((response) => {
+                        return response.text()
+                    })
+                    .then((html) => {
+                        document.getElementById("content").innerHTML = html
+                        console.log(html)
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                    })
+                break;
+        
+            default:
+                console.log(button.id)
+                break;
+        }
+    })
+})
