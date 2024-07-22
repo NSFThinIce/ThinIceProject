5824e682d00dcd063d9868268536a364b5ca1f15
Chris J <shift16>
2024-07-22 16:15:50 -0400

    Fixed the navigation script and updated some pages so that they don't spit out errors

--- scripts/navbar/toggle-navbar-view.js
+++ scripts/navbar/toggle-navbar-view.js
@@ -1,23 +1,25 @@
-// Queries the navigation bar's button and stores it as a constant
-const navBarButton = document.querySelector(".navbar-toggle")
+{
+    // Queries the navigation bar's button and stores it as a constant
+    const navBarButton = document.querySelector(".navbar-toggle")
 
-// Queries the links: Team, News and Publications & Presentations and stores them in a constant
-const navBarLinks = document.querySelector(".navbar>.other-links")
+    // Queries the links: Team, News and Publications & Presentations and stores them in a constant
+    const navBarLinks = document.querySelector(".navbar>.other-links")
 
-// When the button is clicked, the other links are made visible and if it's clicked again, the other content is hidden
-let currentStyle
-navBarButton.addEventListener("click", () => {
-    // Override this variable 
-    currentStyle = navBarLinks.style.display
-    
-    // By default, the style property of the HTML tag is empty so that instance must be accounted for 
-    if (currentStyle === "none" || currentStyle === "") {
-        navBarLinks.style.display = "flex"
-    } else {
-        navBarLinks.style.display = "none"
-    }
-})
+    // When the button is clicked, the other links are made visible and if it's clicked again, the other content is hidden
+    let currentStyle
+    navBarButton.addEventListener("click", () => {
+        // Override this variable 
+        currentStyle = navBarLinks.style.display
 
-window.addEventListener('resize', () => {
-    
-})
+        // By default, the style property of the HTML tag is empty so that instance must be accounted for 
+        if (currentStyle === "none" || currentStyle === "") {
+            navBarLinks.style.display = "flex"
+        } else {
+            navBarLinks.style.display = "none"
+        }
+    })
+
+    window.addEventListener('resize', () => {
+
+    })
+}
--- scripts/page-navigation-handler.js
+++ scripts/page-navigation-handler.js
@@ -1,51 +1,65 @@
-const buttons = document.querySelectorAll(".text-link", ".image-link")
+{
+    // Holds a list of all of the clickable items that should return a new content onto the single page
+    const navBarLinks = document.querySelectorAll(".text-link", ".image-link")
+    
+    // The HTML tag that contains the main content for each sub-page
+    const mainContentElement = document.querySelector(".main-content")
+    
+    // The <link> tag that will be updated when a page is clicked on
+    // It will hold the new CSS for the sub-page
+    const stylesheetLink = document.querySelector('.css-page-link')
 
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        switch (button.id) {
-            case "home-page":
-                fetch("/pages/home/index.html")
-                    .then((response) => {
-                        return response.text()
-                    })
-                    .then((html) => {
-                        document.getElementById("content").innerHTML = html
-                        console.log(html)
-                    })
-                    .catch((err) => {
-                        console.log(err)
-                    })
-                break;
-            case "team-page":
-                fetch("/pages/team/index.html")
-                    .then((response) => {
-                        return response.text()
-                    })
-                    .then((html) => {
-                        document.getElementById("content").innerHTML = html
-                        console.log(html)
-                    })
-                    .catch((err) => {
-                        console.log(err)
-                    })
-                break;
-            case "news-page":
-                fetch("/pages/news/index.html")
-                    .then((response) => {
-                        return response.text()
-                    })
-                    .then((html) => {
-                        document.getElementById("content").innerHTML = html
-                        console.log(html)
-                    })
-                    .catch((err) => {
-                        console.log(err)
-                    })
-                break;
+    // The mapping of each id to a sub-page's directory/folder
+    const pageMapping = {
+        ["home-page"]: "/sub-pages/home",
+        ["team-page"]: "/sub-pages/team",
+        ["news-page"]: "/sub-pages/news",
+        ["pub-page"]: "/sub-pages/pub-and-pre"
+    }
 
-            default:
-                console.log(button.id)
-                break;
+    // Handles the fetching of sub-pages and injecting in the right location
+    function subPageFetcher(pageID) {
+        // Throws an error if the pageID is not listed in the pageMapping object
+        if (!Object.hasOwn(pageMapping, pageID)) {
+            throw new Error(`The page ID ${pageID} does not map to any location.`)
         }
+
+        const mappedLocation = pageMapping[pageID]
+
+        const fetchedContent =
+            fetch(`${mappedLocation}/main.html`)
+                .then(response => {
+                    // If the response to the fetch is not okay, then throw an error with the reason why
+                    if (!response.ok) {
+                        throw new Error(`Response status: ${response.status}`)
+                    } else {
+                        // The content type is checked to ensure that an HTML page is being fetched but!
+                        // If it's not an HTML page then something is wrong
+                        const contentType = response.headers.get("Content-Type")
+
+                        if (contentType.includes("text/html")) {
+                            return response.text()
+                        } else {
+                            throw new Error(`The following url: ${mappedLocation} points to content of type ${contentType} which is not an HTML file`)
+                        }
+                    }
+                })
+                .then(htmlContent => {
+                    // Set the CSS for the sub-page
+                    stylesheetLink.href = `${mappedLocation}/style.css`
+                    
+                    // Append the link to the head of the HTML document
+                    mainContentElement.innerHTML = htmlContent
+                })
+                .catch(err => {
+                    // When an error occurs
+                    console.log(err)
+                })
+    }
+
+    navBarLinks.forEach(link => {
+        link.addEventListener("click", _ => {
+            subPageFetcher(link.id)
+        })
     })
-})
+}
