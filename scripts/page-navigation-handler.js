af91afa33014a162188e049f2d07daf5ad180111
Chris J <shift16>
2024-07-23 17:24:00 -0400

    Made it so sub-pages will continue to appear even when the user manually changes the URL, goes forward or backward by pressing the back and forward buttons, reloads the page, and leaves the page and comes back which is how pages typically work

--- scripts/page-navigation-handler.js
+++ scripts/page-navigation-handler.js
@@ -1,68 +1,112 @@
 {
     // Holds a list of all of the clickable items that should return a new content onto the single page
+    // The below code is pending deletion TODO delete it if it's not necessary
     const navBarLinks = document.querySelectorAll(".text-link", ".image-link")
-    
+
     // The HTML tag that contains the main content for each sub-page
     const mainContentElement = document.querySelector(".main-content")
-    
+
     // The <link> tag that will be updated when a page is clicked on
     // It will hold the new CSS for the sub-page
     const stylesheetLink = document.querySelector('.css-page-link')
 
     // The mapping of each id to a sub-page's directory/folder
     const pageMapping = {
-        ["home-page"]: "/ThinIceProject/sub-pages/home",
-        ["team-page"]: "/ThinIceProject/sub-pages/team",
-        ["news-page"]: "/ThinIceProject/sub-pages/news",
-        ["pub-page"]: "/ThinIceProject/sub-pages/pub-and-pre"
+        ["home-page"]: "/sub-pages/home", // Must be updated before being deployed!
+        ["team-page"]: "/sub-pages/team", // Must be updated before being deployed!
+        ["news-page"]: "/sub-pages/news", // Must be updated before being deployed!
+        ["pub-page"]: "/sub-pages/pub-and-pre" // Must be updated before being deployed!
     }
 
     // Handles the fetching of sub-pages and injecting in the right location
-    function subPageFetcher(pageID) {
-        // Throws an error if the pageID is not listed in the pageMapping object
-        if (!Object.hasOwn(pageMapping, pageID)) {
-            throw new Error(`The page ID ${pageID} does not map to any location.`)
-        }
+    function subPageFetcher(cssPageID) {
+        // Throws an error if the cssPageID is not listed in the pageMapping object
+        if (!Object.hasOwn(pageMapping, cssPageID)) {
+            throw new Error(`The page ID ${cssPageID} does not map to any location.`)
+        } // A 404 should be shown instead!!!! TODO
 
-        const mappedLocation = pageMapping[pageID]
+        const mappedLocation = pageMapping[cssPageID]
 
-        const fetchedContent =
-            fetch(`${mappedLocation}/main.html`)
-                .then(response => {
-                    // If the response to the fetch is not okay, then throw an error with the reason why
-                    if (!response.ok) {
-                        throw new Error(`Response status: ${response.status}`)
+        fetch(`${mappedLocation}/main.html`)
+            .then(response => {
+                // If the response to the fetch is not okay, then throw an error with the reason why
+                if (!response.ok) {
+                    throw new Error(`Response status: ${response.status}`)
+                } else {
+                    // The content type is checked to ensure that an HTML page is being fetched but!
+                    // If it's not an HTML page then something is wrong
+                    const contentType = response.headers.get("Content-Type")
+
+                    if (contentType.includes("text/html")) {
+                        return response.text()
                     } else {
-                        // The content type is checked to ensure that an HTML page is being fetched but!
-                        // If it's not an HTML page then something is wrong
-                        const contentType = response.headers.get("Content-Type")
-
-                        if (contentType.includes("text/html")) {
-                            return response.text()
-                        } else {
-                            throw new Error(`The following url: ${mappedLocation} points to content of type ${contentType} which is not an HTML file`)
-                        }
+                        throw new Error(`The following url: ${mappedLocation} points to content of type ${contentType} which is not an HTML file`)
                     }
-                })
-                .then(htmlContent => {
-                    // Set the CSS for the sub-page
-                    stylesheetLink.href = `${mappedLocation}/style.css`
-                    
-                    // Append the link to the head of the HTML document
-                    mainContentElement.innerHTML = htmlContent
-                })
-                .catch(err => {
-                    // When an error occurs
-                    console.log(err)
-                })
+                }
+            })
+            .then(htmlContent => {
+                // Set the CSS for the sub-page
+                stylesheetLink.href = `${mappedLocation}/style.css`
+
+                // Append the link to the head of the HTML document
+                mainContentElement.innerHTML = htmlContent
+            })
+            .catch(err => {
+                // When an error occurs
+                console.log(err)
+            })
     }
 
-    navBarLinks.forEach(link => {
-        link.addEventListener("click", _ => {
-            subPageFetcher(link.id)
-        })
+    // When one of the links (<a> tags) is clicked on, the subPageFetcher function is called 
+    // This code might not be needed
+    // navBarLinks.forEach(link => {
+    //     link.addEventListener("click", _ => {
+    //         subPageFetcher(link.id)
+    //     })
+    // })
+
+    function fetchPageID(pageID) {
+        // The only cases for the hash that are accepted is when it says:
+        //      #news: news sub-page
+        //      #publications&presentations: publications and presentations sub-page
+        //      #team: the team sub-page
+        //      #home: the home sub-page
+        //      nothing (""): should redirect to the home page by default and the hash must be updated
+        switch (pageID) {
+            case "#news":
+                subPageFetcher("news-page")
+                break
+            case "#publications&presentations":
+                subPageFetcher("pub-page")
+                break
+            case "#team":
+                subPageFetcher("team-page")
+                break
+            case "":
+                window.location.hash = "home" // Sets the hash to #home
+            case "#home":
+                subPageFetcher("home-page")
+                break
+            default:
+                // A 404 should be shown as well! TODO
+                throw new Error(`The page ${pageURL} cannot be automatically fetched. Did you follow the process for adding pages? (Currently does not exist)`)
+        }
+    }
+
+    // When the page loads, the subPageFetcher should set the current sub-page based off the hash (#) in the url
+    document.addEventListener("DOMContentLoaded", _ => {        
+        console.log("WQDQWDQWDQW")
+        // The hash or page ID (#) for the current window is used to determine the current sub-page
+        const pageID = window.location.hash
+        fetchPageID(pageID)
+    })
+
+    // When the user interacts with the page by pressing on a link, pressing on the back button, forward button, etc
+    // The page will be updated (This is necessary so that if the user goes back or forward, the page should update but)
+    // It seems like it's called when other events occur)
+    window.addEventListener("popstate", _ => {
+        // The hash or page ID (#) for the current window is used to determine the current sub-page
+        const pageID = window.location.hash
+        fetchPageID(pageID)
     })
 }
-
-// TODO! Have it so when the page reloads, the content stays
-// Also, have it so that page starts at the sub-page "home"
